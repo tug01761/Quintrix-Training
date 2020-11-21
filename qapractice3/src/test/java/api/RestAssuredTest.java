c36209e961c448e62a530098780ed5a8a0cf5d9b
Mike <tug01761@temple.edu>
2020-11-21 03:25:53 -0500

    Patch and Put API Test

--- qapractice3/src/test/java/api/RestAssuredTest.java
+++ qapractice3/src/test/java/api/RestAssuredTest.java
@@ -3,10 +3,15 @@ package api;
 import static io.restassured.RestAssured.*;
 
 import static org.hamcrest.Matchers.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.testng.annotations.Test;
+
+import org.json.simple.JSONObject;
 
 
 
@@ -48,7 +53,7 @@ public class RestAssuredTest extends FunctionalTest{
 	@Test
 	public void TestingStatusCode204() {
 
-		Object request = new Object();
+		JSONObject request = new JSONObject();
 		given().
 		body(request.toString()).
 		when().
@@ -59,19 +64,41 @@ public class RestAssuredTest extends FunctionalTest{
 		//The request has succeeded but there is nothing to send for the response.
 	}
 
-	/*
+	
 	@Test
 	public void UsingPatchToUpdate() {
-		Map<String, Object> loginCredentials = new HashMap<String, Object>();
-		loginCredentials.put("name", "morpheus2");
+		
+		HashMap<String,Object> request = new HashMap<String,Object>();
+		request.put("name", "morpheus2");
+		
+		JSONObject jsonRequest = new JSONObject(request);
 		
 		
-		given().get(basePath + "/api/users/2").then()
-		.statusCode(200);
-		
+		given().body(jsonRequest.toJSONString())
+		.when()
+		.patch(basePath + "/api/users/2").then().statusCode(200)
+		.assertThat()
+		.body("updatedAt", equalTo(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").format(new Date())));
+	
 		
 	}
-	*/
+	
+	@Test
+	public void UsingPutToUpdate() {
+		
+		HashMap<String,Object> request = new HashMap<String,Object>();
+		request.put("name", "morpheus2");
+		
+		JSONObject jsonRequest = new JSONObject(request);
+		
+		given().body(jsonRequest.toJSONString())
+		.when()
+		.put(basePath + "/api/users/2").then().statusCode(200)
+		.assertThat()
+		.body("updatedAt", equalTo(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").format(new Date())));
+	
+		
+	}
 	
 }
 
