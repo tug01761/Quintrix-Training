c4a732433982ab832495f5ac7844ed2d129d1f8d
Mike <tug01761@temple.edu>
2020-11-26 02:38:32 -0500

    DemoFormPage set commands

--- qapractice3/src/test/java/dataDriven/DemoFormPage.java
+++ qapractice3/src/test/java/dataDriven/DemoFormPage.java
@@ -1,5 +1,180 @@
 package dataDriven;
 
-public class DemoFormPage extends DataDrivenTestBase {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+
+import framework.PageObjectBase;
+
+public class DemoFormPage extends PageObjectBase {
+	
+
+	
+
+	
+	public DemoFormPage(WebDriver driver, String url) {
+		super(driver, url);
+	}
+	
+	@FindBy(id="firstName")
+	WebElement firstNameTextBox;
+	
+	@FindBy(id="lastName")
+	WebElement lastNameTextBox;
+	
+	@FindBy(id="userEmail")
+	WebElement emailTextBox;
+	
+	@FindBy(id="gender-radio-1")
+	WebElement maleGenderRadio;
+	
+	@FindBy(id="gender-radio-2")
+	WebElement femaleGenderRadio;
+	
+	@FindBy(id="gender-radio-3")
+	WebElement otherGenderRadio;
+	
+	@FindBy(id="userNumber")
+	WebElement mobileNumberTextBox;
+	
+	@FindBy(id="dateOfBirthInput")
+	WebElement dobTextBox;
+	
+	@FindBy(id="subjectsInput")
+	WebElement subjectsTextBox;
+	
+	@FindBy(id="hobbies-checkbox-1")
+	WebElement sportsCheckbox;
+	
+	@FindBy(id="hobbies-checkbox-2")
+	WebElement readingCheckbox;
+	
+	@FindBy(id="hobbies-checkbox-3")
+	WebElement musicCheckbox;
+	
+	@FindBy(id="currentAddress")
+	WebElement addressTextbox;
+
+	
+	public DemoFormPage navigate() {
+		super.navigate("");
+		return this;
+	}
+
+	public DemoFormPage setFirstName(String input) {
+		firstNameTextBox.sendKeys(input);
+		return this;
+	}
+	
+	public DemoFormPage setLastName(String input) {
+		lastNameTextBox.sendKeys(input);
+		return this;
+	}
+	
+	public DemoFormPage setEmail(String input) {
+		emailTextBox.sendKeys(input);
+		return this;
+	}
+
+	public DemoFormPage setGender(String input) {
+		
+		Actions actions = new Actions(driver);
+		
+		if(input == "Male")
+		{
+			actions.moveToElement(maleGenderRadio).click().build().perform();
+			//maleGenderRadio.click();
+		}
+		else if(input == "Female")
+		{
+			actions.moveToElement(femaleGenderRadio).click().build().perform();
+			//femaleGenderRadio.click();
+		}
+		else if(input =="Other")
+		{
+			actions.moveToElement(otherGenderRadio).click().build().perform();
+			//otherGenderRadio.click();
+		}
+		
+		return this;
+	}
+
+	public DemoFormPage setMobileNumber(String input) {
+		mobileNumberTextBox.sendKeys(input);
+		return this;
+	}
+	
+	public DemoFormPage setDOB(String input) {
+
+		String Old_Format = "MM/dd/yyyy";
+		String New_Format = "dd MMMM yyyy";
+		String newDateString;
+		
+		SimpleDateFormat sdf = new SimpleDateFormat(Old_Format);
+		
+		try {
+			Date d = sdf.parse(input);
+			sdf.applyPattern(New_Format);
+			newDateString = sdf.format(d);
+			dobTextBox.sendKeys(newDateString);
+			for(int i = 0; i < newDateString.length(); i++)
+			{
+				dobTextBox.sendKeys(Keys.ARROW_LEFT);
+			}
+			for(int i = 0; i < 11; i++)
+			{
+				dobTextBox.sendKeys(Keys.BACK_SPACE);
+			}
+			dobTextBox.sendKeys(Keys.ENTER);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		return this;
+	}
+	
+
+	public DemoFormPage setSubjects(String input) {
+		subjectsTextBox.sendKeys(input);
+		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		subjectsTextBox.sendKeys(Keys.ENTER);
+		
+		return this;
+	}
+
+	public DemoFormPage setHobbies(String input) {
+Actions actions = new Actions(driver);
+		
+		if(input == "Sports")
+		{
+			actions.moveToElement(sportsCheckbox).click().build().perform();
+			//maleGenderRadio.click();
+		}
+		else if(input == "Reading")
+		{
+			actions.moveToElement(readingCheckbox).click().build().perform();
+			//femaleGenderRadio.click();
+		}
+		else if(input =="Music")
+		{
+			actions.moveToElement(musicCheckbox).click().build().perform();
+			//otherGenderRadio.click();
+		}
+		
+		return this;
+	}
+
+	public DemoFormPage setAddress(String input) {
+		addressTextbox.sendKeys(input);
+		return this;
+	}
+	
 }
--- qapractice3/src/test/java/dataDriven/ParameterDriven.java
+++ qapractice3/src/test/java/dataDriven/ParameterDriven.java
@@ -2,22 +2,20 @@ package dataDriven;
 
 import org.testng.annotations.Test;
 
-import framework.TestBase;
-
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
 
-public class ParameterDriven extends TestBase {
+public class ParameterDriven extends DataDrivenTestBase {
 
 	//https://demoqa.com/automation-practice-form
 	
 	@DataProvider(name = "student-list")
 	public Student[] dataProviderMethod() {
 		Student[] studentObject = new Student[3];
-		studentObject[0] = new Student("Mike", "Lauw", "mlauw@gmail.com", "Male", "2654333111", "05/01/1996", "Computer Science", "Reading", "2210 South Street", "NCR", "Noida");
-		studentObject[1] = new Student("Bob", "Troy", "btroy@gmail.com", "Male", "1234567890", "10/12/2000", "Computer Science", "Reading", "110 North Street", "NCR", "Noida");
-		studentObject[2] = new Student("Kathy", "White", "kwhite@gmail.com", "Female", "5234954032", "12/25/2003", "Computer Science", "Reading", "200 Wolf Street", "NCR", "Noida");
+		studentObject[0] = new Student("Mike", "Lauw", "mlauw@gmail.com", "Male", "2654333111", "05/01/1996", "Arts", "Reading", "2210 South Street", "NCR", "Noida");
+		studentObject[1] = new Student("Bob", "Troy", "btroy@gmail.com", "Other", "1234567890", "10/12/2000", "Maths", "Sports", "110 North Street", "NCR", "Noida");
+		studentObject[2] = new Student("Kathy", "White", "kwhite@gmail.com", "Female", "5234954032", "12/25/2003", "Computer Science", "Music", "200 Wolf Street", "NCR", "Noida");
 		
 		
         return studentObject;
@@ -25,10 +23,25 @@ public class ParameterDriven extends TestBase {
 
 	@Test(dataProvider = "student-list")
 	public void registerStudents(Student student) {
-		String expectedFirstName = student.getFirstName();
+		//String expectedFirstName = student.getFirstName();
 		
-		//String submittedFirstName = new DemoFormPage(driver, baseUrl);
-				
+		new DemoFormPage(driver, baseUrl)
+				.navigate()
+				.setFirstName(student.getFirstName())
+				.setLastName(student.getLastName())
+				.setEmail(student.getEmail())
+				.setGender(student.getGender())
+				.setMobileNumber(student.getMobileNumber())
+				.setDOB(student.getDOB())
+				.setSubjects(student.getSubject())
+				.setHobbies(student.getHobbies())
+				.setAddress(student.getAddress());
+				/*
+				.setState()
+				.setCity()
+				.submit()
+				.getConfirmation();
+				*/
 		
 		
 		
@@ -36,10 +49,12 @@ public class ParameterDriven extends TestBase {
 
 	@BeforeTest
 	public void beforeTest() {
+		super.beforeTest();
 	}
 
 	@AfterTest
 	public void afterTest() {
+		//super.afterTest();
 	}
 
 }
